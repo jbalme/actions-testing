713811d51b8c30a23abd8ced46baf093999dea4e
Jonathan Baldwin <jbal@jbal.me>
2022-09-11 22:13:29 -0400

    y

--- create-repo.sh
+++ create-repo.sh
@@ -2,6 +2,7 @@
 
 sudo dnf install -y --skip-broken createrepo
 mkdir -p /data/repo
+ls -R /data/packages
 find /data/packages -iname '*.rpm' -type f -exec mv -t /data/repo \{\} \;
 createrepo /data/repo
 chmod -R u=wrX,go=rX /data/repo
